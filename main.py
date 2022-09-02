a37e333be3695e0aedfb9777141b621494ab0d79
MariusB5 <marius.briscut5@gmail.com>
2022-09-02 14:14:07 -0400

    some changes

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# I have no idea what to put here
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+PATH = Service("C:\\Users\\mariu\\chromedriver.exe")
+driver = webdriver.Chrome(service=PATH)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ test1_ab_testing.py
@@ -0,0 +1,10 @@
+import main
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import unittest
+from selenium.webdriver.common.by import By
+import time
+
+
+if __name__ == '__main__':
+    unittest.main()
