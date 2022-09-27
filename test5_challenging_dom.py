14a1927bd00bb1016b9707b6b0868e07f45ebda3
MariusB5 <marius.briscut5@gmail.com>
2022-09-27 11:17:35 -0400

    finished challenging dom

--- test5_challenging_dom.py
+++ test5_challenging_dom.py
@@ -12,7 +12,16 @@ title = "Challenging DOM"
 text = "The hardest part in automated web testing is finding the best locators (e.g., ones that well named, unique, and unlikely to change). It's more often than not that the application you're testing was not built with this concept in mind. This example demonstrates that with unique IDs, a table with no helpful locators, and a canvas element."
 button_list = ["foo", "bar", "baz", "qux"]
 header_list = ["Lorem", "Ipsum", "Dolor", "Sit", "Amet", "Diceret", "Action"]
-content_list = ["Iuvaret0", "Apeirian0", "Adipisci0", "Definiebas0", "Consequuntur0", "Phaedrum0"]
+content_list0 = ["Iuvaret0", "Apeirian0", "Adipisci0", "Definiebas0", "Consequuntur0", "Phaedrum0"]
+content_list1 = ["Iuvaret1", "Apeirian1", "Adipisci1", "Definiebas1", "Consequuntur1", "Phaedrum1"]
+content_list2 = ["Iuvaret2", "Apeirian2", "Adipisci2", "Definiebas2", "Consequuntur2", "Phaedrum2"]
+content_list3 = ["Iuvaret3", "Apeirian3", "Adipisci3", "Definiebas3", "Consequuntur3", "Phaedrum3"]
+content_list4 = ["Iuvaret4", "Apeirian4", "Adipisci4", "Definiebas4", "Consequuntur4", "Phaedrum4"]
+content_list5 = ["Iuvaret5", "Apeirian5", "Adipisci5", "Definiebas5", "Consequuntur5", "Phaedrum5"]
+content_list6 = ["Iuvaret6", "Apeirian6", "Adipisci6", "Definiebas6", "Consequuntur6", "Phaedrum6"]
+content_list7 = ["Iuvaret7", "Apeirian7", "Adipisci7", "Definiebas7", "Consequuntur7", "Phaedrum7"]
+content_list8 = ["Iuvaret8", "Apeirian8", "Adipisci8", "Definiebas8", "Consequuntur8", "Phaedrum8"]
+content_list9 = ["Iuvaret9", "Apeirian9", "Adipisci9", "Definiebas9", "Consequuntur9", "Phaedrum9"]
 canvas_size = {'height': 202, 'width': 601}
 
 
@@ -55,8 +64,46 @@ class ChallengingDOM(unittest.TestCase):
     def test_table_content(self):
         driver = self.driver
         table_content = driver.find_elements(by=By.TAG_NAME, value="td")
+        y = 0
         for x in range(0, 6):
-            self.assertEqual(table_content[x].text, content_list[x])
+            self.assertEqual(table_content[x].text, content_list0[y])
+            y += 1
+        y = 0
+        for x in range(7, 13):
+            self.assertEqual(table_content[x].text, content_list1[y])
+            y += 1
+        y = 0
+        for x in range(14, 20):
+            self.assertEqual(table_content[x].text, content_list2[y])
+            y += 1
+        y = 0
+        for x in range(21, 27):
+            self.assertEqual(table_content[x].text, content_list3[y])
+            y += 1
+        y = 0
+        for x in range(28, 34):
+            self.assertEqual(table_content[x].text, content_list4[y])
+            y += 1
+        y = 0
+        for x in range(35, 41):
+            self.assertEqual(table_content[x].text, content_list5[y])
+            y += 1
+        y = 0
+        for x in range(42, 48):
+            self.assertEqual(table_content[x].text, content_list6[y])  
+            y += 1
+        y = 0
+        for x in range(49, 55):
+            self.assertEqual(table_content[x].text, content_list7[y]) 
+            y += 1
+        y = 0      
+        for x in range(56, 62):
+            self.assertEqual(table_content[x].text, content_list8[y])
+            y += 1
+        y = 0
+        for x in range(63, 69):
+            self.assertEqual(table_content[x].text, content_list9[y])
+            y += 1
 
     def test_canvas_element(self):
         driver = self.driver
--- test_debug.py
+++ test_debug.py
@@ -13,12 +13,19 @@ from selenium.webdriver.common.alert import Alert
 from pynput.keyboard import Key, Controller
 
 PATH = Service("C:\\Users\\mariu\\chromedriver.exe")
-driver = webdriver.Chrome(service=PATH)
-driver.get("https://the-internet.herokuapp.com/broken_images")
-driver.maximize_window()
-driver.quit()
+url = "https://the-internet.herokuapp.com/"
 
-content_list = ["Iuvaret0", "Apeirian0", "Adipisci0", "Definiebas0", "Consequuntur0", "Phaedrum0"]
+content_list0 = ["Iuvaret0", "Apeirian0", "Adipisci0", "Definiebas0", "Consequuntur0", "Phaedrum0"]
+content_list1 = ["Iuvaret1", "Apeirian1", "Adipisci1", "Definiebas1", "Consequuntur1", "Phaedrum1"]
+content_list2 = ["Iuvaret2", "Apeirian2", "Adipisci2", "Definiebas2", "Consequuntur2", "Phaedrum2"]
+content_list3 = ["Iuvaret3", "Apeirian3", "Adipisci3", "Definiebas3", "Consequuntur3", "Phaedrum3"]
+content_list4 = ["Iuvaret4", "Apeirian4", "Adipisci4", "Definiebas4", "Consequuntur4", "Phaedrum4"]
+content_list5 = ["Iuvaret5", "Apeirian5", "Adipisci5", "Definiebas5", "Consequuntur5", "Phaedrum5"]
+content_list6 = ["Iuvaret6", "Apeirian6", "Adipisci6", "Definiebas6", "Consequuntur6", "Phaedrum6"]
+content_list7 = ["Iuvaret7", "Apeirian7", "Adipisci7", "Definiebas7", "Consequuntur7", "Phaedrum7"]
+content_list8 = ["Iuvaret8", "Apeirian8", "Adipisci8", "Definiebas8", "Consequuntur8", "Phaedrum8"]
+content_list9 = ["Iuvaret9", "Apeirian9", "Adipisci9", "Definiebas9", "Consequuntur9", "Phaedrum9"]
+
 
 class ChallengingDOM(unittest.TestCase):
     def setUp(self):
@@ -32,9 +39,47 @@ class ChallengingDOM(unittest.TestCase):
     def test_table_content(self):
         driver = self.driver
         table_content = driver.find_elements(by=By.TAG_NAME, value="td")
+        y = 0
         for x in range(0, 6):
-            self.assertEqual(table_content[x].text, content_list[x])
-                    
+            self.assertEqual(table_content[x].text, content_list0[y])
+            y += 1
+        y = 0
+        for x in range(7, 13):
+            self.assertEqual(table_content[x].text, content_list1[y])
+            y += 1
+        y = 0
+        for x in range(14, 20):
+            self.assertEqual(table_content[x].text, content_list2[y])
+            y += 1
+        y = 0
+        for x in range(21, 27):
+            self.assertEqual(table_content[x].text, content_list3[y])
+            y += 1
+        y = 0
+        for x in range(28, 34):
+            self.assertEqual(table_content[x].text, content_list4[y])
+            y += 1
+        y = 0
+        for x in range(35, 41):
+            self.assertEqual(table_content[x].text, content_list5[y])
+            y += 1
+        y = 0
+        for x in range(42, 48):
+            self.assertEqual(table_content[x].text, content_list6[y])  
+            y += 1
+        y = 0
+        for x in range(49, 55):
+            self.assertEqual(table_content[x].text, content_list7[y]) 
+            y += 1
+        y = 0      
+        for x in range(56, 62):
+            self.assertEqual(table_content[x].text, content_list8[y])
+            y += 1
+        y = 0
+        for x in range(63, 69):
+            self.assertEqual(table_content[x].text, content_list9[y])
+            y += 1
+
     def tearDown(self):
         self.driver.quit()
     
