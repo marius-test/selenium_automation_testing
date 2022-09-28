7b0590de8fc1b8eed34ca23925c2e59b8c028077
MariusB5 <marius.briscut5@gmail.com>
2022-09-28 17:04:14 -0400

    finished and debugged checkboxes test

--- test6_checkboxes.py
+++ test6_checkboxes.py
@@ -11,13 +11,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.alert import Alert
 
 PATH = Service("C:\\Users\\mariu\\chromedriver.exe")
 url = "https://the-internet.herokuapp.com/"
-# driver = webdriver.Chrome(Service=PATH)
-# action_chains = ActionChains(driver)
-# alert = Alert(driver)
+text = "checkbox 1\ncheckbox 2"
 
 
 class Checkboxes(unittest.TestCase):
@@ -28,10 +25,32 @@ class Checkboxes(unittest.TestCase):
         driver.maximize_window()
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, "h1")))
         driver.find_element(by=By.XPATH, value="//a[normalize-space()='Checkboxes']").click()
-        
-    def test1(self):
-        self.assertEqual(1, 1)
-    
+
+    def test_text(self):
+        driver = self.driver
+        checkbox_string = driver.find_element(by=By.TAG_NAME, value="form").text
+        self.assertEqual(checkbox_string, text)
+
+    def test_check(self):
+        driver = self.driver
+        checkbox = driver.find_elements(by=By.TAG_NAME, value="input")
+        if checkbox[0].is_selected() == False:
+            checkbox[0].click()
+        self.assertTrue(checkbox[0].is_selected())
+        if checkbox[1].is_selected() == False:
+            checkbox[1].click()
+        self.assertTrue(checkbox[1].is_selected())
+
+    def test_uncheck(self):
+        driver = self.driver
+        checkbox = driver.find_elements(by=By.TAG_NAME, value="input")
+        if checkbox[0].is_selected() == True:
+            checkbox[0].click()
+        self.assertFalse(checkbox[0].is_selected())
+        if checkbox[1].is_selected() == True:
+            checkbox[1].click()
+        self.assertFalse(checkbox[1].is_selected())
+
     def tearDown(self):
         self.driver.quit()
     
