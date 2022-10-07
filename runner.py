ce719b7c62ec9d508b94a7bbb94ecc89f8ac1cbb
MariusB5 <marius.briscut5@gmail.com>
2022-10-07 14:24:42 -0400

    created a runner file

---
+++ runner.py
@@ -0,0 +1,11 @@
+import os
+
+if __name__ == '__main__':
+    os.system('python test0_sanity.py')
+    os.system('python test1_ab_testing.py')
+    os.system('python test2_add_remove_elements.py')
+    os.system('python test3_basic_auth.py')
+    os.system('python test4_broken_images.py')
+    os.system('python test5_challenging_dom.py')
+    os.system('python test6_checkboxes.py')
+    
