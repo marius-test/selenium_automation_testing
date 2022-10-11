6f4f3092128bfcbe43bc56d38c6173080e2bdb43
MariusB5 <marius.briscut5@gmail.com>
2022-10-11 12:01:34 -0400

    some optimizations

--- runner.py
+++ runner.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     os.system('python test5_challenging_dom.py')
     os.system('python test6_checkboxes.py')
     os.system('test7_context_menu.py')
+    os.system('test8_digest_authentication.py')
