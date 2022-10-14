9d01c34f4e78c876a65d048a8227f0514a9f1e52
MariusB5 <marius.briscut5@gmail.com>
2022-10-14 11:29:57 -0400

    test 9 almost finished

--- test9_disappearing_elements.py
+++ test9_disappearing_elements.py
@@ -15,6 +15,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 PATH = Service("C:\\Users\\mariu\\chromedriver.exe")
 url = "https://the-internet.herokuapp.com/"
+header = "Disappearing Elements"
+paragraph = "This example demonstrates when elements on a page change by disappearing/reappearing on each page load."
+expected_buttons = ["Home", "About", "Contact Us", "Portfolio", "Gallery"]
+expected_footer = "Powered by Elemental Selenium"
+expected_footer_link = "http://elementalselenium.com/"
 
 
 class DisappearingElements(unittest.TestCase):
@@ -25,13 +30,45 @@ class DisappearingElements(unittest.TestCase):
         driver.maximize_window()
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.TAG_NAME, "h1")))
         driver.find_element(by=By.XPATH, value="/html/body/div[2]/div/ul/li[9]/a").click()
-    
-    def test_1(self):
+    """
+    def test_header_and_paragraph(self):
         driver = self.driver
+        self.assertEqual(driver.find_element(by=By.TAG_NAME, value="h3").text, header)
+        self.assertEqual(driver.find_element(by=By.TAG_NAME, value="p").text, paragraph)
     
+    def test_buttons_text(self):
+        driver = self.driver
+        list_of_buttons = driver.find_elements(by=By.TAG_NAME, value="li")
+        i = 0
+        for button in list_of_buttons:
+            self.assertEqual(list_of_buttons[i].text, expected_buttons[i])
+            i += 1
+    """
+    def test_buttons_redirect(self):
+        driver = self.driver
+        list_of_buttons = driver.find_elements(by=By.TAG_NAME, value="a")
+        i = 0
+        for buttons in list_of_buttons:
+            print(list_of_buttons[i].get_attribute("href"))
+            i += 1
+        self.assertEqual(list_of_buttons[1].get_attribute("href"), "https://the-internet.herokuapp.com/")
+        self.assertEqual(list_of_buttons[2].get_attribute("href"), "https://the-internet.herokuapp.com/about/")
+        self.assertEqual(list_of_buttons[3].get_attribute("href"), "https://the-internet.herokuapp.com/contact-us/")
+        self.assertEqual(list_of_buttons[4].get_attribute("href"), "https://the-internet.herokuapp.com/portfolio/")
+        
+        # if
+        # self.assertEqual(list_of_buttons[5].get_attribute("href"), "https://the-internet.herokuapp.com/gallery/")
+
+    """
+    def test_footer(self):
+        driver = self.driver
+        footer = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div")
+        link = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div/div/a")
+        self.assertEqual(footer.text, expected_footer)
+        self.assertEqual(link.get_attribute("href"), expected_footer_link)
+    """
     def tearDown(self):
-        pass
-        # self.driver.quit()
+        self.driver.quit()
 
 
 if __name__ == '__main__':
