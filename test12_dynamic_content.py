aa91c80c2fcc26a0557a5f4c77e991673da73570
MariusB5 <marius.briscu5@gmail.com>
2023-10-20 09:48:25 -0400

    small change

--- test12_dynamic_content.py
+++ test12_dynamic_content.py
@@ -13,7 +13,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import TimeoutException
 
 
