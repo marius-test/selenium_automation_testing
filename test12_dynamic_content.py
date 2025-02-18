0a4be71811bc1d9c5bdc460d56896bf26a40d202
Marius Liviu Briscut <marius.briscut5@gmail.com>
2025-02-18 09:06:53 -0400

    added two more tests to test file 12

--- test12_dynamic_content.py
+++ test12_dynamic_content.py
@@ -2,7 +2,7 @@ import unittest
 import urllib3
 import requests
 import pyautogui
-from time import sleep
+import time
 from pynput.keyboard import Key, Controller
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -24,9 +24,10 @@ url = "https://the-internet.herokuapp.com/"
 expected_title = 'Dynamic Content'
 expected_static_text_1 = "Accusantium eius ut architecto neque vel voluptatem vel nam eos minus ullam dolores voluptates enim sed voluptatem rerum qui sapiente nesciunt aspernatur et accusamus laboriosam culpa tenetur hic aut placeat error autem qui sunt."
 expected_static_text_2 = "Omnis fugiat porro vero quas tempora quis eveniet ab officia cupiditate culpa repellat debitis itaque possimus odit dolorum et iste quibusdam quis dicta autem sint vel quo vel consequuntur dolorem nihil neque sunt aperiam blanditiis."
-expected_static_image_1 = "/img/avatars/Original-Facebook-Geek-Profile-Avatar-1.jpg"
-expected_static_image_2 = "/img/avatars/Original-Facebook-Geek-Profile-Avatar-5.jpg"
-expected_dynamic_images = ["/img/avatars/Original-Facebook-Geek-Profile-Avatar-1.jpg", "/img/avatars/Original-Facebook-Geek-Profile-Avatar-3.jpg", "/img/avatars/Original-Facebook-Geek-Profile-Avatar-5.jpg"]
+expected_static_image_1 = "https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-1.jpg"
+expected_static_image_2 = "https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-5.jpg"
+images_numbers = [0, 1, 2, 3, 4, 5, 6, 7]
+expected_dynamic_images = [f"https://the-internet.herokuapp.com/img/avatars/Original-Facebook-Geek-Profile-Avatar-{number}.jpg" for number in images_numbers]
 
 
 class DynamicContent(unittest.TestCase):
@@ -44,19 +45,23 @@ class DynamicContent(unittest.TestCase):
 
     def test_static_text(self):
         self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/p[2]/a").click()
-        self.assertEqual(expected_static_text_1, self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/div[1]/div[2]").text)
-        self.assertEqual(expected_static_text_2, self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/div[2]/div[2]").text)
+        self.assertEqual(expected_static_text_1, self.driver.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[2]').text)
+        self.assertEqual(expected_static_text_2, self.driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[2]').text)
         self.driver.refresh()
-        self.assertEqual(expected_static_text_1, self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/div[1]/div[2]").text)
-        self.assertEqual(expected_static_text_2, self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/div[2]/div[2]").text)
+        self.assertEqual(expected_static_text_1, self.driver.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[2]').text)
+        self.assertEqual(expected_static_text_2, self.driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[2]').text)
 
     def test_static_images(self):
         self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/p[2]/a").click()
-        self.assertEqual(expected_static_image_1, self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/div[1]/div[1]/img").text)
-        self.assertEqual(expected_static_image_2, self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/div[2]/div[1]/img").text)
+        image_1 = self.driver.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[1]/img')
+        image_2 = self.driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[1]/img')
+        image_1_src = image_1.get_attribute('src')
+        image_2_src = image_2.get_attribute('src')
+        self.assertEqual(expected_static_image_1, image_1_src)
+        self.assertEqual(expected_static_image_2, image_2_src)
         self.driver.refresh()
-        self.assertEqual(expected_static_image_1, self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/div[1]/div[1]/img").text)
-        self.assertEqual(expected_static_image_2, self.driver.find_element(By.XPATH, "//*[@id=\"content\"]/div[2]/div[1]/img").text)
+        self.assertEqual(expected_static_image_1, image_1_src)
+        self.assertEqual(expected_static_image_2, image_2_src)
  
     def test_dynamic_text(self):
         pass
