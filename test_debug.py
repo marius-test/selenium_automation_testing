5e9e4986b86d8cbeadf36f530a55f6082fc5bce9
Marius Liviu Briscut <marius.briscut5@gmail.com>
2025-04-09 13:48:17 -0400

    added one import into the debug test

--- test_debug.py
+++ test_debug.py
@@ -16,6 +16,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 # from webdriver_manager.chrome import ChromeDriverManager
+# import seletools
 
 
 
